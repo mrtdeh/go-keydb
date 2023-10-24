6989eb848801dd5f92522d3ae30e70c4468a5622
mrtdeh <mrtdeh@gmail.com>
2023-10-24 14:45:17 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mrtdeh/go-keydb
+
+go 1.20
+
+require github.com/redis/rueidis v1.0.20
---
+++ keydb.handler.go
@@ -0,0 +1,120 @@
+package keydb
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/redis/rueidis"
+)
+
+type Client struct {
+	conn rueidis.Client
+}
+
+var client *Client
+
+var ctx = context.Background()
+
+type PubData []byte
+
+var pub sync.RWMutex
+
+type Config struct {
+	Host      string
+	Port      int
+	Pass      string
+	DB        int
+	TLSConfig *tls.Config
+}
+
+type KeyVal struct {
+	Key string
+	Val string
+}
+
+func Init(opt Config) error {
+	pub.Lock()
+	defer pub.Unlock()
+
+	if client != nil {
+		return nil
+	}
+
+	redisopt := rueidis.ClientOption{
+		InitAddress: []string{fmt.Sprintf("%s:%d", opt.Host, opt.Port)},
+		Password:    opt.Pass,
+		TLSConfig:   opt.TLSConfig,
+		SelectDB:    opt.DB,
+	}
+
+	rueidisClient, err := rueidis.NewClient(redisopt)
+	if err != nil {
+		return fmt.Errorf("error in connecting to redis : %s", err.Error())
+	}
+
+	res := rueidisClient.Do(ctx, rueidisClient.B().Ping().Build())
+	if res.Error() != nil {
+		return fmt.Errorf("redis ping failed : %s", res.Error().Error())
+	}
+
+	client = &Client{
+		conn: rueidisClient,
+	}
+
+	log.Println("successfuly connect to redis server")
+	return nil
+}
+
+func SetMulti(kvs []KeyVal) error {
+	pub.Lock()
+	defer pub.Unlock()
+
+	var cmds = make(rueidis.Commands, 0, len(kvs))
+	for _, m := range kvs {
+		cmd := client.conn.B().Set().Key(m.Key).Value(m.Val).Build()
+		cmds = append(cmds, cmd)
+	}
+	for _, resp := range client.conn.DoMulti(ctx, cmds...) {
+		if err := resp.Error(); err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
+}
+
+func Publish(chanel string, msgs []PubData) error {
+	pub.Lock()
+	defer pub.Unlock()
+
+	var cmds = make(rueidis.Commands, 0, len(msgs))
+
+	for _, m := range msgs {
+		cmds = append(cmds, client.conn.B().Publish().Channel(chanel).Message(string(m)).Build().Pin())
+	}
+
+	for _, resp := range client.conn.DoMulti(ctx, cmds...) {
+		if err := resp.Error(); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	return nil
+}
+
+func Subscribe(msgChan chan interface{}, chanel ...string) {
+
+	cmd := client.conn.B().Subscribe().Channel(chanel...).Build()
+	go func() {
+		err := client.conn.Receive(ctx, cmd, func(msg rueidis.PubSubMessage) {
+			// msgCh <- msg.Message
+			msgChan <- msg.Message
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+}
